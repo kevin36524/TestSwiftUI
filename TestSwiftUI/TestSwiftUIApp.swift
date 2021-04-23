e636312a202bde755bd7372999e2f8ba6947c12a
Kevin Patel <patelkev@verizonmedia.com>
2021-04-22 17:01:52 -0700

    Initial Commit

---
+++ TestSwiftUI/TestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftUIApp.swift
+//  TestSwiftUI
+//
+//  Created by Kevin Patel on 4/22/21.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

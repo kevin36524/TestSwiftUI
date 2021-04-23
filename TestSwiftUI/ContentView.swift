2f37ddbb49782c0907bc503343b5c447cda13a1a
Kevin Patel <patelkev@verizonmedia.com>
2021-04-22 18:08:23 -0700

    -->  Added the programmatic navigation to Settings screen    - First just show the navigationView    - Next do it programatically
    -->  Added local counter to the SettingsView and see how it is reset on every back and forth
    
    -->  Added the AppState with User and counter as the fields.
     - Used environmentObject
     - Demo ObservableObject, Bindings,
     - Added CounterView and saw how you can pass with environment object

--- TestSwiftUI/ContentView.swift
+++ TestSwiftUI/ContentView.swift
@@ -8,9 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isSettingsViewShown: Bool = false
+    @EnvironmentObject var appState: AppState
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+            NavigationView {
+                
+                VStack {
+                    Text("Hello, \(appState.user.name)!")
+                        .padding()
+                    Button(action: navigateToSettings) {
+                        Text("Settings")
+                    }
+                    
+                    NavigationLink(destination: SettingsView(globalCount: $appState.globalCount), isActive: $isSettingsViewShown) {
+                    }
+                    
+                    CounterView()
+                }
+            }
+    }
+    
+    func navigateToSettings() {
+        print("KEVINDEBUG I will navigate to settings")
+        isSettingsViewShown = true
+    }
+}
+
+struct CounterView: View {
+    
+    @EnvironmentObject var appState: AppState
+    
+    var body: some View {
+        VStack {
+            Text("Global Count \(appState.globalCount)").padding()
+            Button(action: {
+                appState.globalCount += 1
+            }, label: {
+                Text("Global Increment")
+            })
+        }
+    }
+}
+
+struct SettingsView: View {
+    @State var localCount = 0
+    @Binding var globalCount: Int
+    
+    var body: some View {
+        VStack {
+            Text("Settings").padding()
+            Text("Local Count \(localCount)").padding()
+            Button(action: {
+                localCount += 1
+                globalCount += 1
+            }, label: {
+                Text("Inc Local")
+            })
+            CounterView()
+        }
+        
     }
 }
 
@@ -19,3 +76,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+    
--- TestSwiftUI/TestSwiftUIApp.swift
+++ TestSwiftUI/TestSwiftUIApp.swift
@@ -7,11 +7,25 @@
 
 import SwiftUI
 
+
+class AppState: ObservableObject {
+    @Published var user: User = User()
+    @Published var globalCount: Int = 0
+}
+
+struct User {
+    var name: String
+    
+    init(name: String = "Kevin") {
+        self.name = name
+    }
+}
+
 @main
 struct TestSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environmentObject(AppState())
         }
     }
 }
